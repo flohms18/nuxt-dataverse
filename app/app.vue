874360a4908c25df4b236d37ab808365fc683190
flohms18 <trouilletflorent@outlook.com>
2026-03-11 14:50:56 +0100

    Added Hello World Vue Template

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <HelloWorld></HelloWorld>
   </div>
 </template>
---
+++ app/components/HelloWorld.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        This is my hello world template
+    </div>
+</template>
