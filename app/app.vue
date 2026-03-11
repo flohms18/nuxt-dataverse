ea45e6f35e39870d069ed88a22fd97ec90653775
flohms18 <trouilletflorent@outlook.com>
2026-03-11 15:05:25 +0100

    Added Index Page

--- app/app.vue
+++ app/app.vue
@@ -6,6 +6,6 @@ import HelloWorld from './components/HelloWorld.vue';
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <HelloWorld></HelloWorld>
+    <NuxtPage />
   </div>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="js">
+</script>
+
+<template>
+    <div>
+        <AppHeader></AppHeader>
+        <h1>Home Page</h1>
+    </div>
+</template>
